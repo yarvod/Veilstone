1265d336e9235eebe340c73dcf21a2e468b23545
yarvod <yar.vod.ole@gmail.com>
2026-07-02 07:39:03 +0300

    chore: release v0.0.1-beta1

--- src/voxel_sandbox/version.py
+++ src/voxel_sandbox/version.py
@@ -2,6 +2,6 @@ from __future__ import annotations
 
 __all__ = ["RELEASE_TAG", "REPO_SLUG", "__version__"]
 
-__version__ = "0.0.1b0"
-RELEASE_TAG = "v0.0.1-beta"
+__version__ = "0.0.1b1"
+RELEASE_TAG = "v0.0.1-beta1"
 REPO_SLUG = "yarvod/Veilstone"
